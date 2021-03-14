b010ea61163b8c1c6aea0aced649272854017807
tkdals2317 <tkdals2317@gmail.com>
2021-03-14 20:01:48 +0900

    주석 추가

--- 7주차 스터디/BOJ_S1_7576_토마토/Main_S1_7576_토마토_이상민_solved.java
+++ 7주차 스터디/BOJ_S1_7576_토마토/Main_S1_7576_토마토_이상민_solved.java
@@ -10,22 +10,17 @@ public class Main_S1_7576_토마토_이상민_solved {
 	static class Point{
 		int pi;
 		int pj;
-		int time;
-		
+		int time;	
 		public Point(int pi, int pj, int time) {
 			super();
 			this.pi = pi;
 			this.pj = pj;
 			this.time = time;
 		}
-
 		@Override
 		public String toString() {
 			return "[" + pi + ", " + pj + ", " + time + "]";
-		}
-
-
-			
+		}		
 	}
 	
 	public static void main(String[] args) throws Exception{
@@ -49,9 +44,13 @@ public class Main_S1_7576_토마토_이상민_solved {
 		br.close();			
 	}
 	static void bfs() {
+		//bfs를 구현하기 위한 queue
 		ArrayDeque<Point> queue = new ArrayDeque<Point>();
+		//몇일째인지 저장하기 위한 time 변수
 		int time = 0;
+		//토마토가 모두 익었는지 안익었는지 확인하기 위한 flag
 		boolean flag = false;
+		//동시에 시작하기 위해 처음에 익은 토마토 영역 queue에 삽입
 		for (int i = 0; i < M; i++) {
 			for (int j = 0; j < N; j++) {
 				if(map[i][j]==1) {
@@ -62,18 +61,23 @@ public class Main_S1_7576_토마토_이상민_solved {
 		}
 		while(!queue.isEmpty()) {
 			Point current = queue.poll();
+			//시간 갱신
 			time = current.time;
+			//4방 탐색
 			for (int d = 0; d < di.length; d++) {
 				int ni = current.pi + di[d];
 				int nj = current.pj + dj[d];
+				//범위 체크 및 방문 체크
 				if(ni>=0 && ni<M && nj>=0 && nj < N && map[ni][nj]==0 && !v[ni][nj]) {
+					//현재 시간에 1을 더하여 queue에 삽입
 					queue.offer(new Point(ni,nj, current.time+1));
+					//방문처리 poll하는 부분에서 하면 메모리 초과 나므로 주의~! 
 					v[ni][nj] = true;
 					map[ni][nj] = 1;
 				}			
 			}					
 		}
-		
+		//안익은 토마토가 있다면 flag를 true로 변경
 		for (int i = 0; i < M; i++) {
 			for (int j = 0; j < N; j++) {
 				if(map[i][j]==0) {
@@ -81,6 +85,7 @@ public class Main_S1_7576_토마토_이상민_solved {
 				}
 			}
 		}
+		//토마토가 다익었다면 false -> time 출력, 안익은 토마토가 있다면  true -> -1 출력
 		if(flag) {
 			System.out.println(-1);
 		}else {
