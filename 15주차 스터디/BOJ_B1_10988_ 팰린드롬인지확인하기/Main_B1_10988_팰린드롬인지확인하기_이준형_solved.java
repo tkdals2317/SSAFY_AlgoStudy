f978fb4b289436555ea242adca4e9274225a3ee8
sean <sean@LAPTOP-CS4GQ6GL>
2021-05-12 18:43:48 +0900

    주석 추가

--- 15주차 스터디/BOJ_B1_10988_ 팰린드롬인지확인하기/Main_B1_10988_팰린드롬인지확인하기_이준형_solved.java
+++ 15주차 스터디/BOJ_B1_10988_ 팰린드롬인지확인하기/Main_B1_10988_팰린드롬인지확인하기_이준형_solved.java
@@ -11,7 +11,7 @@ public class Main_bj_10988_팰린드롬인지확인하기_구미_4_이준형 {
 		
 		int ans=1;
 		int len=str.length();
-		for(int i=0;i<len/2;i++) {
+		for(int i=0;i<len/2;i++) {	//앞과 뒤에서 부터 수 확인
 			if(str.charAt(i)!=str.charAt(len-i-1)) {
 				ans=0;
 				break;
--- 15주차 스터디/BOJ_B1_1157_단어공부/Main_B1_1157_단어공부_이준형_solved.java
+++ 15주차 스터디/BOJ_B1_1157_단어공부/Main_B1_1157_단어공부_이준형_solved.java
@@ -11,22 +11,22 @@ public class Main_bj_1157_단어공부_구미_4_이준형 {
 		int[] bae=new int[30];
 		int max=0;
 		for(int i=0;i<str.length();i++) {
-			int tmp=str.charAt(i)-'A';
-			if(tmp>26)
+			int tmp=str.charAt(i)-'A';	//대문자 형변환
+			if(tmp>26)	//소문자 인지 판단
 				tmp+='A'-'a';
-			bae[tmp]++;
-			max=Math.max(max, bae[tmp]);
+			bae[tmp]++;	//해당위치 값 증가
+			max=Math.max(max, bae[tmp]);	//최대값 갱신
 		}
 		
 		int count=0;
 		int idx=-1;
 		for(int i=0;i<26;i++) {
-			if(max==bae[i]) {
+			if(max==bae[i]) {	//최댓값 찾으면 증가
 				count++;
 				idx=i;
 			}
 		}
-		if(count==1)
+		if(count==1)	//최대 값이 유일하면 증가
 			System.out.println((char)('A'+idx));
 		else
 			System.out.println("?");
