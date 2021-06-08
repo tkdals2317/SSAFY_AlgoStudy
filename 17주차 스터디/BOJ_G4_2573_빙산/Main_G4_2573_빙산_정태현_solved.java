8e7964f39e61bda752665be6d69f100caf1ba969
jth <jth2747@gmail.com>
2021-06-09 00:10:45 +0900

    주석달았슴

--- 17주차 스터디/BOJ_G4_2573_빙산/Main_G4_2573_빙산_정태현_solved.java
+++ 17주차 스터디/BOJ_G4_2573_빙산/Main_G4_2573_빙산_정태현_solved.java
@@ -55,7 +55,7 @@ public class Main_G4_2573_빙산_정태현_solved {
 		
 		// -------Problem Solving -------
 		while(iceCnt() < 2) {
-			//시간초과 원인
+			//빙산  수가 0개면 그냥 그대로 녹아서 없어지는 거라서 0을 출력
 			if(iceCnt() == 0) {
 				yearcnt = 0;
 				break;
@@ -74,6 +74,7 @@ public class Main_G4_2573_빙산_정태현_solved {
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
 				if(!visited[i][j] && map[i][j] != 0) {
+					//dfs로 빙하 개수 카운트
 					dfs(i, j , visited);
 					cnt++;
 				} 
@@ -99,10 +100,14 @@ public class Main_G4_2573_빙산_정태현_solved {
 	}
 
 	private static void bfs() {
+		//process 이전 빙산이었는데 녹아서 0이 된것은 바다로 치지 않기 때문에 visited 사용
 		boolean[][] visited = new boolean[N][M];
+		
+		//현재 년도의 빙산수만큼 돌기 위함
 		int size = q.size();
 		for (int i = 0; i < size; i++) {
 			Pos cur = q.poll();
+			//해당 칸은 원래 빙산이었음을 나타내기 위해
 			visited[cur.r][cur.c] = true;
 			
 			for (int j = 0; j < dr.length; j++) {
@@ -115,12 +120,14 @@ public class Main_G4_2573_빙산_정태현_solved {
 					}
 				}
 				
+				//녹는 과정에서 0이하가 되어버리면 더 녹일 필요 없이 break
 				if(map[cur.r][cur.c] <= 0) {
 					map[cur.r][cur.c] = 0;
 					break;
 				}
 			}
 			
+			//다음 년도 process를 위해 0이상 빙산인 것들을 q에 넣음
 			if(map[cur.r][cur.c] > 0) {
 				q.offer(new Pos(cur.r, cur.c));
 			}
