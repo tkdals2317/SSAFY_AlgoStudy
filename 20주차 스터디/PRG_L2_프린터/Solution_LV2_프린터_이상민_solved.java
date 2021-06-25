3f4738c5641cb456b6b5efcd4609082f503cc2be
tkdals2317 <tkdals2317@gmail.com>
2021-06-25 22:46:15 +0900

    왜 커밋 안올라감?

--- 20주차 스터디/PRG_L2_프린터/Solution_LV2_프린터_이상민_solved.java
+++ 20주차 스터디/PRG_L2_프린터/Solution_LV2_프린터_이상민_solved.java
@@ -13,6 +13,7 @@ class Solution_LV2_프린터_이상민_solved {
         }
         
         public int compareTo(Word o){
+        	//중요도가 높은 순서로 queue에서 나옴(순서 주의) 
             return Integer.compare(o.priority, this.priority);
         }
 
@@ -23,6 +24,7 @@ class Solution_LV2_프린터_이상민_solved {
 
         System.out.println(Arrays.toString(priorities));
         PriorityQueue<Word> queue = new PriorityQueue<Word>();
+        
         for(int i=0; i<priorities.length; i++){
             queue.offer(new Word(i, priorities[i]));
         }
@@ -30,9 +32,13 @@ class Solution_LV2_프린터_이상민_solved {
         while(!queue.isEmpty()){
             System.out.println(queue);
             for(int i=0; i<priorities.length; i++){
+            	
                 if(queue.peek().priority==priorities[i]){
+                	//i의 priorities와 중요도가 같으면 출력 
                     System.out.println(queue.poll()+" "+i);
+                    //소요된 시간 증가
                     answer++;
+                    //만약 요청한 문서의 번호와 방금 큐에서 뺀 인덱스가 일치하다면 종료
                     if(location==i){
                         queue.clear();
                         break;
