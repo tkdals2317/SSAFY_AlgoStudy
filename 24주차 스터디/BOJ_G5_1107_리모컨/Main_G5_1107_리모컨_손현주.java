3e2395cd81cebc4b70faa90f7e63afb6e8b11f52
HyeonJu Son <kkobukk95@gmail.com>
2021-08-25 17:57:07 +0900

    손반성

--- 24주차 스터디/BOJ_G5_1107_리모컨/Main_G5_1107_리모컨_손현주.java
+++ 24주차 스터디/BOJ_G5_1107_리모컨/Main_G5_1107_리모컨_손현주.java
@@ -1,64 +1,49 @@
-/*
- * n의 최대값이 500000이므로 0-9까지의 버튼으로 만들 수 있는 최대 번호는 999999이다.
- * 가능성은 두 경우로 나눈다. 
- * 			1) +, - 로만 이동했을 때 -> 그냥 Math.abs(N-100)하면된다.
- * 			2) 가장 차이 적은 위치 +, -로 했을 때
- * 				- 완탐으로 0~999999까지 숫자를 만들어서 차이를 계산.
- * 			3) 찾는 수가 100일 때 -> 젤 좋지.
- * 1. n과 비교해서 n과의 차이가 가장 작은 애를 저장한다.
- * 		(비교하는 수는 한자리씩 고장여부를 검사한다.)
- * 2. Math.abs(가장가까운 수 - N) + 자리값 
- */
-import java.io.*;
 import java.util.*;
-
-public class bj_1107_G5 {
-
-	static int N;
-	static boolean[] broken ;
-
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine()); // 찾으려고 하는 목표 타겟
-		broken = new boolean[10]; // 0 - 9 까지의 버튼 중 고장난 애 true
-		int brokeCnt = Integer.parseInt(br.readLine()); // 고장난 개수
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < brokeCnt; ++i) {
-			broken[Integer.parseInt(st.nextToken())] = true; // 고장났으면 true로 해준다.
-		}
-		if(N==100) System.out.println(0); // N = 100이면 그냥 바로 0 출력
-		else System.out.println(process());//과정처리 후 출력
-	}
-
-
-	static int process() {
-		int min = Integer.MAX_VALUE; // 최소값
-		String now = "", close=""; // 현재 만들어본 숫자 값, 가장 가까운 값
-		
-		// 1. 그냥 바로 빼보기 
-		int plusMinus = Math.abs(N-100);
-		// 2. 0 ~ 999999에서 N과 가장 가까운 수를 찾아보기
-		for(int i=0;i<1000000;++i) {
-			now = Integer.toString(i);//현재 만들어본 숫자를 string으로 저장
-			int nowCnt = Math.abs(N-Integer.parseInt(now));//현재 횟수
-			if(!isBroken(now) && min > nowCnt) { // 고장난 버튼이 없고 더 적은 횟수를 가지면
-				min = nowCnt; // 최소횟수를 갱신해준다.
-				close = now; // 근접한 값을 갱신해준다.
-			}
-		}
-		// 3. 더 작은 횟수 비교해서 출력해준다.
-		if(close.equals("")) return plusMinus; // 근접한 값을 찾지 못했으면 plusMinus반환
-		else // 근접한 값이 있으면 plusMinus랑 비교해서 더작은 값 반환
-			return Math.min(plusMinus, Math.abs(N-Integer.parseInt(close)) + close.length());
-	}
-	
-	static boolean isBroken(String val) { // 고장난 버튼이 포함되어있는지 반환해주는 메소드
-		for(int i=0;i<val.length();++i) { // 조합한 숫자 하나하나 검사해보기
-			int num = val.charAt(i)-'0';
-			if(num >=10 || num<0) continue;
-			if(broken[num]) return true; // 고장난 버튼 포함 시 true 반환
-		}
-		return false; // 고장난 것 없으면 false 반환
-	}
-
+public class Main{
+public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String n = scan.next();
+        int m = scan.nextInt();
+        int [] a = new int[10]; //고장난 버튼을 표시할 배열
+        
+        for(int i=0; i<m; i++)  //고장난 버튼엔 -1을 넣는다.
+            a[scan.nextInt()] = -1;
+ 
+        if(n.equals("100")) 
+            System.out.println(0);
+        else {
+            int min = Integer.MAX_VALUE;
+            String v = "";//0 ~ 1000000까지의 숫자를 문자열화 하기위한~
+            String closer = ""; //리모컨으로 입력할 수 있는 n과 가장 가까운 채널
+            
+            for(int i = 0; i<1000000; i++) {
+                boolean isOk = true;
+                v = i+""; //숫자를 문자열로 저장
+                for(int j=0; j<v.length(); j++) { //고장난 버튼이 포함되어 있는지 검사
+                    if(a[v.charAt(j)-'0'] == -1) { 
+                        isOk = false;
+                        break;
+                    }
+                }
+                if(isOk) { //v에 고장난 버튼이 없으면 가장 가까운 채널을 찾는다. n-v로  + 또는 - 버튼을 몇번 눌러야하는지 계산
+                    if(min > Math.abs(Integer.parseInt(n)-Integer.parseInt(v))) { 
+                        min = Math.abs(Integer.parseInt(n)-Integer.parseInt(v));
+                        closer = v; 
+                    }
+                }
+            }
+            
+            int result1 = Math.abs(Integer.parseInt(n)-100); //result1에는 현재채널인 100에서 +와-만으로 n까지 갈때의 횟수를 저장
+            if(closer.equals("")) //가장 가까운 수가 비어있으면 그냥 result1출력
+                System.out.println(result1);
+            else { //result2에는 n-closer로 closer에서 n까지 가기위한 횟수에 closer를 누르기위한 버튼 입력 횟수인 closer의 길이를 더한다.
+                int result2 = Math.abs(Integer.parseInt(n)-Integer.parseInt(closer))+closer.length(); 
+                if(result1>result2)
+                    System.out.println(result2);
+                else
+                    System.out.println(result1);
+            }
+        }
+    }
 }
+
