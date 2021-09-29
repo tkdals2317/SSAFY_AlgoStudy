73ef468174366aeefb34659b6f8a15fe3dac12b8
jth2747 <jth2747@gmail.com>
2021-09-29 18:07:04 +0900

    외않됌

---
+++ 27주차 스터디/BOJ_G2_1655_가운데를말해요/Main_G2_1655_가운데를말해요_정태현_X.java
@@ -0,0 +1,14 @@
+package algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_G2_1655_가운데를말해요_정태현_X {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+	}
+}
