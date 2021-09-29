a9b701a2abeca0c0322bba048348044beb1b8e40
jth2747 <jth2747@gmail.com>
2021-09-29 18:07:26 +0900

    외않됌

--- 27주차 스터디/BOJ_G2_1655_가운데를말해요/Main_G2_1655_가운데를말해요_정태현_X.java
+++ 27주차 스터디/BOJ_G2_1655_가운데를말해요/Main_G2_1655_가운데를말해요_정태현_X.java
@@ -1,14 +1,40 @@
-package algorithm.boj;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
-public class Main_G2_1655_가운데를말해요_정태현_X {
-	public static void main(String[] args) throws IOException {
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
 		
+		PriorityQueue<Integer> minQ = new PriorityQueue<>();
+		PriorityQueue<Integer> maxQ = new PriorityQueue<>(Collections.reverseOrder());
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(br.readLine());
+			
+			if(maxQ.size() == minQ.size()) {
+				maxQ.add(num);
+				
+				if(!minQ.isEmpty() && maxQ.peek() > minQ.peek()) {
+					minQ.add(maxQ.poll());
+					maxQ.add(minQ.poll());
+				}
+			}
+			else {
+				minQ.offer(num);
+				
+				if(maxQ.peek() > minQ.peek()) {
+					minQ.add(maxQ.poll());
+					maxQ.add(minQ.poll());
+				}
+			}
+			
+			System.out.println(maxQ.peek());
+		}
 	}
 }
